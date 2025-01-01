c7563c680bb643cbd6aa2a1847d7cc1c87dbfccf
nilspettersson <nils-pettsson@outlook.com>
2025-01-01 14:01:42 +0100

    Added logic for moving with arrow keys and better selection of nodes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
-	import { tick } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	import { SvelteMap, SvelteSet } from 'svelte/reactivity';
 
 	type Node = {
@@ -16,8 +16,12 @@
 	nodes.set('A4', { value: '2024-12-26' });
 	nodes.set('B4', { value: '140' });
 	nodes.set('B5', { value: '=SUM(B2:B4)' });
+	console.log(getKey(1, 1));
 
 	let selectedNodes = new SvelteSet<string>(['0,0']);
+
+	let activeNode = $state<[number, number]>([3, 3]);
+	let editing = $state(false);
 
 	function getKey(row: number, col: number) {
 		const rowName = row;
@@ -50,17 +54,46 @@
 			return sum;
 		}
 	}
+
+	onMount(() => {
+		document.addEventListener('keydown', async (e) => {
+			if (editing) return;
+
+			editing = false;
+			if (e.key === 'ArrowUp') {
+				activeNode[0]--;
+			} else if (e.key === 'ArrowDown') {
+				activeNode[0]++;
+			} else if (e.key === 'ArrowLeft') {
+				activeNode[1]--;
+			} else if (e.key === 'ArrowRight') {
+				activeNode[1]++;
+			} else if (e.key === 'Enter') {
+				editing = true;
+				await tick();
+				const element = document.getElementById(getKey(...activeNode)) as
+					| HTMLInputElement
+					| undefined;
+
+				if (element) {
+					element.focus();
+				}
+			}
+		});
+	});
 </script>
 
-<div class="flex h-0 w-full grow flex-col overflow-auto border-border/60 p-4">
-	{#each Array.from({ length: 100 }) as _, row}
+<div class="flex h-0 w-full grow flex-col overflow-hidden border-border/60 p-4">
+	{#each Array.from({ length: 10 }) as _, row}
+		{console.log('row')}
 		<div class="flex w-min first:border-t">
 			<button
 				class={cn(
 					'flex h-6 w-12 items-center justify-center border-b border-l border-border text-right text-xs font-semibold last:border-r'
-				)}>{row}</button
-			>
-			{#each Array.from({ length: 100 }) as _, col}
+				)}
+				>{row}
+			</button>
+			{#each Array.from({ length: 10 }) as _, col}
 				{#if row === 0}
 					<button
 						class={cn(
@@ -69,34 +102,68 @@
 					>
 				{:else}
 					<button
-						onclick={async (event) => {
-							selectedNodes.add(getKey(row, col));
-							const element = event.currentTarget;
+						ondblclickcapture={async (e) => {
+							if (editing) return;
+							editing = true;
+							activeNode = [row, col];
+
+							const element = e.currentTarget;
 							if (element.firstElementChild) {
 								await tick();
-								(element.firstElementChild as HTMLInputElement).focus();
+								const input = element.firstElementChild as HTMLInputElement | undefined;
+								if (input) {
+									input.focus();
+								}
 							}
 						}}
-						onkeypress={async (event) => {}}
-						onfocusin={(event) => {}}
+						onclick={async (event) => {
+							if (editing) return;
+							activeNode = [row, col];
+							editing = false;
+						}}
+						onkeypress={async (e) => {
+							if (editing) return;
+							editing = true;
+							activeNode = [row, col];
+
+							const element = e.currentTarget;
+							if (element.firstElementChild) {
+								await tick();
+								const input = element.firstElementChild as HTMLInputElement | undefined;
+								if (input) {
+									input.value = '';
+									input.focus();
+								}
+							}
+						}}
+						onfocusout={(e) => {
+							//console.log('focusout', getKey(row, col));
+							/*nodes.set(getKey(row, col), { value: e.currentTarget.value });
+							editing = false;*/
+						}}
 						class={cn(
+							'focus-within:outline-none',
 							'flex h-6 w-24 items-center justify-end border-b border-l border-border text-right text-xs font-semibold last:border-r',
 							nodes.has(getKey(row, col)) &&
 								isNaN(parseInt(nodes.get(getKey(row, col))!.value)) &&
-								'justify-start'
+								'justify-start',
+							getKey(...activeNode) === getKey(row, col) && 'border-2 border-primary'
 						)}
 					>
-						{#if selectedNodes.has(getKey(row, col))}
+						{#if getKey(...activeNode) === getKey(row, col) && editing}
 							<input
-								value={nodes.get(getKey(row, col)) ? nodes.get(getKey(row, col))!.value : ''}
+								id={getKey(row, col)}
+								value={nodes.get(getKey(row, col))?.value ?? ''}
 								type="text"
 								class="h-full w-full p-1"
 								onfocusout={(e) => {
-									selectedNodes.delete(getKey(row, col));
+									//selectedNodes.delete(getKey(row, col));
+									editing = false;
 								}}
 								onchange={(e) => {
 									nodes.set(getKey(row, col), { value: e.currentTarget.value });
-									selectedNodes.delete(getKey(row, col));
+									//selectedNodes.delete(getKey(row, col));
+									editing = false;
 								}}
 							/>
 						{:else if nodes.get(getKey(row, col))}
